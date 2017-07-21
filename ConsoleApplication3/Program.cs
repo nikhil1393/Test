f0e48112e5e75412e05c27d10b11f15a164dc52f
awasthin <awasthin@VDI-TC2-ITD-018>
2017-07-21 14:15:55 +0530

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+  class Program
+    {
+       static void Main(string[] args)
+        {
+            byte op1 = 1;
+            sbyte op2 = -1;
+            var result = op1 & op2;
+            Console.WriteLine(result.GetType().Name);
+            Console.ReadKey();
+        }
+    }
+}
